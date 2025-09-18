62e4927386b6c6a962a4e27a5d3a4beef2ffd49e
Muhammad Uwais Baig <premierhub.com@gmail.com>
2025-09-18 21:53:01 +0500

    WEEK 1 - CONDITIONALS
    Uploaded solutions for Week 1 of CS50’s Introduction to Python.
    
    Includes:
    - bank.py → Implemented logic to return responses based on greetings.
    - deep.py → Built a program to validate responses against a set phrase.
    - extensions.py → Categorized file types using string parsing and conditionals.
    - interpreter.py → Created a basic arithmetic interpreter using conditional logic.
    
    These exercises strengthen understanding of conditionals, string handling, 
    and input validation in Python.

---
+++ extensions.py
@@ -0,0 +1,16 @@
+text = input("File name: ").strip().lower()
+
+if text.endswith(".gif"):
+    print("image/gif")
+elif text.endswith(".jpg") or text.endswith(".jpeg"):
+    print("image/jpeg")
+elif text.endswith(".png"):
+    print("image/png")
+elif text.endswith(".pdf"):
+    print("application/pdf")
+elif text.endswith(".txt"):
+    print("text/plain")
+elif text.endswith(".zip"):
+    print("application/zip")
+else:
+    print("application/octet-stream")
